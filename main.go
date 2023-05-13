f81a3f64d76d6a676aeee14d9589db18103380ac
Yash RV <55505724+YashV1729@users.noreply.github.com>
2023-05-13 11:09:43 +0530

    Update main.go

--- main.go
+++ main.go
@@ -19,10 +19,7 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	fileServer := http.FileServer(http.Dir("./static"))
-	http.Handle("/", fileServer)
 	http.HandleFunc("/hello", helloHandler)
-
 	fmt.Printf("Starting server at port 8080\n")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
